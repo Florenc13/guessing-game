376b83ad5348dc8fbe455463c6c3df8525f8761d
Florenc13 <khisyametdinov.ruslan@gmail.com>
2023-02-24 15:05:50 +0300

    feat: add completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.nMax = max;
+        this.nMin = min;
     }
 
     guess() {
-
+        this.result = Math.round((this.nMin + this.nMax) / 2)
+        return this.result
     }
 
     lower() {
-
+        this.nMax = this.result;
     }
 
     greater() {
-
+        this.nMin = this.result;
     }
 }
 
